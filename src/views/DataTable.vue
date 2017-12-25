35c3cf920e29298a6e02fd03afe6d4b9d2887b21
Айрат Жаншуаков <dakanca@gmail.com>
2017-12-25 22:01:34 +0600

    feat: add datatable

--- src/views/DataTable.vue
+++ src/views/DataTable.vue
@@ -9,6 +9,28 @@
                 Таблица
               </h2>
             </div>
+            <div class="field has-addons">
+              <p class="control">
+                <span class="select">
+                  <b-select v-model="searchCategory" placeholder="Выбирите одну">
+                      <option
+                          v-for="(option, index) in searchFields"
+                          :value="option.machine_name"
+                          :key="index">
+                          {{ option.name }}
+                      </option>
+                  </b-select>
+                </span>
+              </p>
+              <p class="control is-expanded">
+                <b-input v-model="search"></b-input>
+              </p>
+              <p class="control">
+                <button @click="onSearch" class="button is-primary">
+                  Поиск
+                </button>
+              </p>
+            </div>
             <b-table :data="dataCollection" 
               :striped="true" 
               :narrowed="true"
@@ -20,10 +42,14 @@
               :per-page="perPage"
               @page-change="onPageChange"
 
-              default-sort="user.first_name">
+              backend-sorting
+              :default-sort-direction="defaultSortOrder"
+              :default-sort="[sortField, sortOrder]"
+              @sort="onSort">
 
               <template slot-scope="props">
-                <b-table-column label="Бюджетная программа"
+                <b-table-column 
+                  label="Бюджетная программа"
                   width="220">
                     {{ props.row.budget_programm | truncate(80) }}
                 </b-table-column>
@@ -48,16 +74,16 @@
                     {{ props.row.administrator_budget_program }}
                 </b-table-column>
 
-                <b-table-column label="Год" numeric>
+                <b-table-column field="year" label="Год" numeric sortable>
                     {{ props.row.year }}
                 </b-table-column>
-                <b-table-column label="2016" numeric>
+                <b-table-column field="budget_2016" label="2016" numeric sortable>
                     {{ props.row.budget_2016 }}
                 </b-table-column>
-                <b-table-column label="2017" numeric>
+                <b-table-column field="budget_2017" label="2017" numeric sortable>
                     {{ props.row.budget_2017 }}
                 </b-table-column>
-                <b-table-column label="2018" numeric>
+                <b-table-column field="budget_2018" label="2018" numeric sortable>
                     {{ props.row.budget_2018 }}
                 </b-table-column>
               </template>
@@ -89,11 +115,25 @@ export default {
       dataCollection: [],
       total: 0,
       loading: false,
-      sortField: 'vote_count',
+      sortField: 'year',
       sortOrder: 'desc',
       defaultSortOrder: 'desc',
       page: 1,
       perPage: 20,
+
+      search: '',
+      searchCategory: 'budget_programm',
+      searchFields: [
+        { name: 'Бюджетная программа', machine_name: 'budget_programm' },
+        { name: 'Вид соц реализаций', machine_name: 'vid_soc_real' },
+        { name: 'Вид гос сод', machine_name: 'vid_gos_sod' },
+        { name: 'Текущие или развитие', machine_name: 'tekushee_ili_razvitie' },
+        { name: 'Администратор бюджетной программы', machine_name: 'administrator_budget_program' }
+        // { name: 'year', machine_name: 'year' },
+        // { name: 'budget_2016', machine_name: 'budget_2016' },
+        // { name: 'budget_2017', machine_name: 'budget_2017' },
+        // { name: 'budget_2018', machine_name: 'budget_2018' }
+      ],
 
       ParseData: Parse.Object.extend('Programme'),
       ParseQuery: null
@@ -121,6 +161,11 @@ export default {
         )
       this.ParseQuery.limit(this.perPage)
       this.ParseQuery.skip((this.page * this.perPage) - this.perPage)
+      if (this.sortOrder === 'asc') {
+        this.ParseQuery.ascending(this.sortField)
+      } else {
+        this.ParseQuery.descending(this.sortField)
+      }
 
       this.ParseQuery.count({
         success (count) {
@@ -160,6 +205,58 @@ export default {
     onPageChange (page) {
       this.page = page
       this.loadAsyncData()
+    },
+    /*
+    * Handle sort event
+    */
+    onSort (field, order) {
+      this.sortField = field
+      this.sortOrder = order
+      this.loadAsyncData()
+    },
+    /**
+     * Handle search request
+     */
+    onSearch () {
+      const ctx = this
+      this.loading = true
+      if (this.sortOrder === 'asc') {
+        this.ParseQuery.ascending(this.sortField)
+      } else {
+        this.ParseQuery.descending(this.sortField)
+      }
+
+      this.ParseQuery.count({
+        success (count) {
+          let currentTotal = count
+          if (count / ctx.perPage > 1000) {
+            currentTotal = ctx.perPage * 1000
+          }
+          ctx.total = currentTotal
+        },
+        error (error) {
+          console.log(error)
+        }
+      })
+      this.ParseQuery.contains(this.searchCategory, this.search)
+      this.ParseQuery.find().then((results) => {
+        let data = []
+        for (let i = 0; i < results.length; i++) {
+          data.push({
+            budget_programm: results[i].get('budget_programm'),
+            vid_soc_real: results[i].get('vid_soc_real'),
+            vid_gos_sod: results[i].get('vid_gos_sod'),
+            tekushee_ili_razvitie: results[i].get('tekushee_ili_razvitie'),
+            administrator_budget_program: results[i].get('administrator_budget_program'),
+            year: results[i].get('year'),
+            budget_2016: results[i].get('budget_2016'),
+            budget_2017: results[i].get('budget_2017'),
+            budget_2018: results[i].get('budget_2018')
+          })
+        }
+        ctx.dataCollection = data
+        this.loading = false
+      })
     }
   },
 
